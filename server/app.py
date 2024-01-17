9cff8c29275de4897535dd5f64b46da80aaba06d
Madison <mlinkenmeyer@gmail.com>
2024-01-16 17:01:53 -0800

    Goals model and class created.

--- server/app.py
+++ server/app.py
@@ -3,13 +3,14 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
-# Add your model imports
 
+# Add your model imports
+from models import Goal
 
 # Views go here!
 
@@ -17,6 +18,15 @@ from config import app, db, api
 def index():
     return '<h1>Krystle and Madi\'s Project Server</h1>'
 
+class Goals(Resource):
+    def get(self):
+        try:
+            goals = [goal.to_dict() for goal in Goal.query.all()]
+            return make_response(goals, 200)
+        except Exception as e:
+            return make_response({"error": str(e)}, 500)
+
+api.add_resource(Goals, "/goals")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
