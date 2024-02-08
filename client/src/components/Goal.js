c6955afb36b05b3cd7e6c39dbe41aa20f6a61f8e
Madison <mlinkenmeyer@gmail.com>
2024-02-08 10:11:21 -0800

    Add edit and delete buttons on Goal component.

--- client/src/components/Goal.js
+++ client/src/components/Goal.js
@@ -4,6 +4,8 @@ function Goal({ goal: { id, title, description, status, targetDate } }) {
   return (
     <div>
       {title}, {description}, {status}, {targetDate}
+      <button>Edit</button>
+      <button>Delete</button>
     </div>
   );
 }
