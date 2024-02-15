74545ed065656feec10a47904a784743970fa339
Madison <mlinkenmeyer@gmail.com>
2024-02-15 11:06:56 -0800

    GoalForm component created separately from GoalsPage

---
+++ client/src/components/GoalForm.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+
+export const GoalForm = ({ goals, setGoals }) => {
+  const [goalsFormValues, setGoalsFormValues] = useState({
+    title: "",
+    description: "",
+    status: "",
+    category: "",
+    target_date: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://127.0.0.1:5555/goals", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(goalsFormValues),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create goal");
+      }
+
+      const newGoal = await response.json();
+
+      setGoals([...goals, newGoal]);
+
+      setGoalsFormValues({
+        title: "",
+        description: "",
+        status: "",
+        category: "",
+        target_date: "",
+      });
+    } catch (error) {
+      console.error("Error creating goal:", error.message);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Create a new goal</h2>
+        <label>
+          Title
+          <input
+            type="text"
+            value={goalsFormValues.title}
+            onChange={(e) =>
+              setGoalsFormValues({ ...goalsFormValues, title: e.target.value })
+            }
+          />
+        </label>
+        <br />
+        <label>
+          Description
+          <input
+            type="text"
+            value={goalsFormValues.description}
+            onChange={(e) =>
+              setGoalsFormValues({
+                ...goalsFormValues,
+                description: e.target.value,
+              })
+            }
+          />
+        </label>
+        <br />
+        <label>
+          Category
+          <input
+            type="text"
+            value={goalsFormValues.category}
+            onChange={(e) =>
+              setGoalsFormValues({
+                ...goalsFormValues,
+                category: e.target.value,
+              })
+            }
+          />
+        </label>
+        <br />
+        <label></label>
+        <label>
+          Status
+          <input
+            type="text"
+            value={goalsFormValues.status}
+            onChange={(e) =>
+              setGoalsFormValues({ ...goalsFormValues, status: e.target.value })
+            }
+          />
+        </label>
+        <br />
+        <label>
+          Target Date
+          <input
+            type="text"
+            value={goalsFormValues.target_date}
+            onChange={(e) =>
+              setGoalsFormValues({
+                ...goalsFormValues,
+                target_date: e.target.value,
+              })
+            }
+          />
+        </label>
+        <br />
+        <button type="submit">Add Goal</button>
+      </form>
+    </div>
+  );
+};
+
+export default GoalForm;
--- client/src/components/GoalsPage.js
+++ client/src/components/GoalsPage.js
@@ -1,16 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Goal from "./Goal";
+import GoalForm from "./GoalForm";
 
 function GoalsPage() {
   const [goals, setGoals] = useState([]);
-
-  const [goalsFormValues, setGoalsFormValues] = useState({
-    title: "",
-    description: "",
-    status: "",
-    category: "",
-    target_date: "",
-  });
+  const [showGoalForm, setShowGoalForm] = useState(false);
 
   useEffect(() => {
     const fetchGoals = async () => {
@@ -21,55 +15,7 @@ function GoalsPage() {
     fetchGoals().catch(console.error);
   }, []);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch("http://127.0.0.1:5555/goals", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(goalsFormValues),
-      });
-
-      if (!response.ok) {
-        throw new Error("Failed to create goal");
-      }
-
-      const newGoal = await response.json();
-
-      setGoals([...goals, newGoal]);
-
-      setGoalsFormValues({
-        title: "",
-        description: "",
-        status: "",
-        category: "",
-        target_date: "",
-      });
-    } catch (error) {
-      console.error("Error creating goal:", error.message);
-    }
-  };
-
-  // function handleDeleteGoal(id) {
-  //   console.log(`Deleting goal with id: ${id}`);
-  //   fetch(`/goals/${id}`, { method: "DELETE" })
-  //     .then((response) => {
-  //       console.log("Response status:", response.status);
-  //       if (response.ok) {
-  //         console.log("Response is ok");
-  //         setGoals((prevGoals) => prevGoals.filter((goal) => goal.id !== id));
-  //       } else {
-  //         console.error("Failed to delete goal");
-  //         throw new Error("Failed to delete goal");
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error deleting goal:", error);
-  //     });
-  // }
+  console.log(goals);
 
   const deleteGoal = (deleteGoal) => {
     const updatedGoals = goals.filter((goal) => goal.id !== deleteGoal.id);
@@ -88,82 +34,11 @@ function GoalsPage() {
     />
   ));
 
-  console.log("Goals:", goals);
-  console.log("Goals List:", goalsList);
-
   return (
     <div>
       <h1>Goals</h1>
       {goalsList}
-      <form onSubmit={handleSubmit}>
-        <h2>Create a new goal</h2>
-        <label>
-          Title
-          <input
-            type="text"
-            value={goalsFormValues.title}
-            onChange={(e) =>
-              setGoalsFormValues({ ...goalsFormValues, title: e.target.value })
-            }
-          />
-        </label>
-        <br />
-        <label>
-          Description
-          <input
-            type="text"
-            value={goalsFormValues.description}
-            onChange={(e) =>
-              setGoalsFormValues({
-                ...goalsFormValues,
-                description: e.target.value,
-              })
-            }
-          />
-        </label>
-        <br />
-        <label>
-          Category
-          <input
-            type="text"
-            value={goalsFormValues.category}
-            onChange={(e) =>
-              setGoalsFormValues({
-                ...goalsFormValues,
-                category: e.target.value,
-              })
-            }
-          />
-        </label>
-        <br />
-        <label></label>
-        <label>
-          Status
-          <input
-            type="text"
-            value={goalsFormValues.status}
-            onChange={(e) =>
-              setGoalsFormValues({ ...goalsFormValues, status: e.target.value })
-            }
-          />
-        </label>
-        <br />
-        <label>
-          Target Date
-          <input
-            type="text"
-            value={goalsFormValues.target_date}
-            onChange={(e) =>
-              setGoalsFormValues({
-                ...goalsFormValues,
-                target_date: e.target.value,
-              })
-            }
-          />
-        </label>
-        <br />
-        <button type="submit">Add Goal</button>
-      </form>
+      <GoalForm />
     </div>
   );
 }
