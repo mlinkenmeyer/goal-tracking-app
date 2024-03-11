255ce3ac845f58088a0e6dbde4ecf1328872c223
Krystle Lee <l33.krys@gmail.com>
2024-03-11 12:35:12 -0700

    applying formik and yup to user form

--- client/src/components/UserForm.js
+++ client/src/components/UserForm.js
@@ -8,14 +8,14 @@ function UserForm({ user, addUser, editUser, showUserForm, setShowUserForm, setS
     const formSchema = yup.object().shape({
         name: yup.string().required("First name is required"),
         email: yup.string().required("Must enter a valid email"),
-        password: yup.string().required("Must enter a password with at least 6 characters & include at least 1 letter and 1 number"),
-        confirm_password: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required("Must match password")
+        password: user ? "" : yup.string().required("Must enter a password with at least 6 characters & include at least 1 letter and 1 number"),
+        confirm_password: user ? "" : yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required("Must match password")
       });
-    
+
     const formik = useFormik({
         initialValues: {
-            name: "",
-            email: "",
+            name: user ? user.name : "",
+            email: user ? user.email : "",
             password: "",
             confirm_password: ""
         },
@@ -26,6 +26,24 @@ function UserForm({ user, addUser, editUser, showUserForm, setShowUserForm, setS
                         email: values.email,
                         password: values.password
                         }    
+            user ?
+            fetch(`/users/${user.id}`, {
+                method: "PATCH",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(userData, null, 2),
+            })
+            .then((r) => r.json())
+            .then((existingUser) => 
+                {
+                editUser(existingUser)
+                // setUserObj(initialValues)
+                setShowEditForm(false)
+                }
+            )
+            :
             fetch("/users", {
                 method: "POST",
                 mode: "cors",
@@ -42,77 +60,77 @@ function UserForm({ user, addUser, editUser, showUserForm, setShowUserForm, setS
             )
     }});
     
-    // const initialValues = {
-    //     name: "",
-    //     email: "",
-    //     password: "",
-    //     confirm_password: ""
-    // }
-    // const [userObj, setUserObj] = useState(user ? user : formik.initialValues)
+    const initialValues = {
+        name: "",
+        email: "",
+        password: "",
+        confirm_password: ""
+    }
+    const [userObj, setUserObj] = useState(user ? user : formik.initialValues)
 
-    // const handleAddUser = (e) => {
-    //     e.preventDefault()
-    //     if (userObj.name.length > 0 && userObj.email.length > 0 && userObj.password === userObj.confirm_password) {
-    //         fetch("/users", {
-    //             method: "POST",
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //                 Accept: "application/json"
-    //             },
-    //             body: JSON.stringify({
-    //                 name: userObj.name,
-    //                 email: userObj.email,
-    //                 password: userObj.password
-    //             })
-    //         })
-    //         .then((r) => r.json())
-    //         .then((newUser) => {
-    //             addUser(newUser)
-    //             setUserObj(formik.initialValues)
-    //             setShowUserForm(false)
-    //             }
-    //         )
-    //     } else {
-    //         alert("All fields are required or passwords don't match")
-    //     }
-    // }
+    const handleAddUser = (e) => {
+        e.preventDefault()
+        if (userObj.name.length > 0 && userObj.email.length > 0 && userObj.password === userObj.confirm_password) {
+            fetch("/users", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json"
+                },
+                body: JSON.stringify({
+                    name: userObj.name,
+                    email: userObj.email,
+                    password: userObj.password
+                })
+            })
+            .then((r) => r.json())
+            .then((newUser) => {
+                addUser(newUser)
+                setUserObj(initialValues)
+                setShowUserForm(false)
+                }
+            )
+        } else {
+            alert("All fields are required or passwords don't match")
+        }
+    }
 
-    // const handleEditUser = (e) => {
-    //     e.preventDefault()
-    //     if (userObj.name.length > 0 && userObj.email.length > 0 && userObj.password === userObj.confirm_password) {
-    //         fetch(`/users/${user.id}`, {
-    //             method: "PATCH",
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //                 Accept: "application/json"
-    //             },
-    //             body: JSON.stringify({
-    //                 name: userObj.name,
-    //                 email: userObj.email,
-    //                 password: userObj.password
-    //             })
-    //         })
-    //         .then((r) => r.json())
-    //         .then((existingUser) => 
-    //             {
-    //             editUser(existingUser)
-    //             setUserObj(formik.initialValues)
-    //             setShowEditForm(false)
-    //             }
-    //         )
-    //     } else {
-    //         alert("All fields are required or passwords don't match")
-    //     }
-    // }
+    const handleEditUser = (e) => {
+        e.preventDefault()
+        if (userObj.name.length > 0 && userObj.email.length > 0 && userObj.password === userObj.confirm_password) {
+            fetch(`/users/${user.id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json"
+                },
+                body: JSON.stringify({
+                    name: userObj.name,
+                    email: userObj.email,
+                    password: userObj.password
+                })
+            })
+            .then((r) => r.json())
+            .then((existingUser) => 
+                {
+                editUser(existingUser)
+                setUserObj(initialValues)
+                setShowEditForm(false)
+                }
+            )
+        } else {
+            alert("All fields are required or passwords don't match")
+        }
+    }
 
-    // const handleSubmitUser = (e) => {
-    //     e.preventDefault()
-    //     if (user) {
-    //         handleEditUser(e)
-    //     } else {
-    //         handleAddUser(e)
-    //     }
-    // }
+    const handleSubmitUser = (e) => {
+        e.preventDefault()
+        if (user) {
+            handleEditUser(e)
+        } else {
+            handleAddUser(e)
+        }
+    }
 
     return (
         <div>
@@ -125,6 +143,7 @@ function UserForm({ user, addUser, editUser, showUserForm, setShowUserForm, setS
                         value={formik.values.name}
                         onChange={formik.handleChange}
                         />
+                        <p> {formik.errors.name}</p>
                         <br/>
                 <label>Email</label>
                     <input 
@@ -134,6 +153,7 @@ function UserForm({ user, addUser, editUser, showUserForm, setShowUserForm, setS
                         value={formik.values.email}
                         onChange={formik.handleChange}
                         />
+                        <p> {formik.errors.email}</p>
                     <br/>
                 <label>Password</label>
                     <input 
@@ -143,6 +163,7 @@ function UserForm({ user, addUser, editUser, showUserForm, setShowUserForm, setS
                         value={formik.values.password}
                         onChange={formik.handleChange}
                         />
+                        <p> {formik.errors.password}</p>
                         <br/>
                 <label>Confirm Password</label>
                     <input 
@@ -152,6 +173,7 @@ function UserForm({ user, addUser, editUser, showUserForm, setShowUserForm, setS
                         value={formik.values.confirm_password}
                         onChange={formik.handleChange}
                         />
+                        <p> {formik.errors.confirm_password}</p>
                         <br/>
                 <button>Submit</button><br/>
             </form>
